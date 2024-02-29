f95496c91767e63060a350bc1233659a7b036c50
Sorokolit <ivso.work@gmail.com>
2024-02-29 11:03:31 +0200

    Third commit

--- get_upcoming_birthdays.py
+++ get_upcoming_birthdays.py
@@ -1,29 +1,33 @@
 from datetime import datetime
+from datetime import timedelta
 
 def get_upcoming_birthdays(users: list) -> list:
-    current_date = datetime.today().date()
+    current_date = datetime.today().date() # визначаємо поточну дату
     congratulations = []
-    congratulation_date = current_date
-    for user in users: # додати умову на 7 днів вперед
-        birthday_this_year = datetime.strptime(user['birthday'], '%Y.%m.%d').replace(year=2024).date()
-        if birthday_this_year < current_date:
-            congratulation_date = birthday_this_year.replace(year=current_date.year + 1)
-        else:
-            if birthday_this_year.weekday() == 5:
+    for user in users:
+        congratulation_date = current_date
+        birthday_this_year = datetime.strptime(user['birthday'], '%Y.%m.%d').replace(year=2024).date() # визначаємо день народження цього року
+        if current_date <= birthday_this_year <= current_date + timedelta(days=6): # Беремо дати в найближчі 7 днів з сьогоднішнього
+            if birthday_this_year.weekday() == 5: # якщо день народження (д.н.) припадає на суботу, вітання переносимо на ПН
                 congratulation_date = birthday_this_year.replace(day=birthday_this_year.day + 2)
-            elif birthday_this_year.weekday() == 6:
+            elif birthday_this_year.weekday() == 6: # якщо д.н. припадає на неділю, вітання переносимо на ПН
                 congratulation_date = birthday_this_year.replace(day=birthday_this_year.day + 1)
-            # Розглянути дату на наступний рік???
-        user['birthday'] = datetime.strftime(congratulation_date, '%Y.%m.%d')
-        congratulations.append(user)
+            else:
+                congratulation_date = birthday_this_year # якщо д.н. припадає на інший день тижня, вітаємо в д.н.
+            user['congratulation_date'] = datetime.strftime(congratulation_date, '%Y.%m.%d') # переводимо дату вітання в рядок і додаємо до словника
+            del user['birthday'] # видаляємо зі словника дату дня народження, залишаємо дату вітання
+            congratulations.append(user)
     return congratulations
 
-users = [
-    {"name": "John Doe", "birthday": "1985.01.30"},
-    {"name": "Jane Smith", "birthday": "1990.02.24"}
-]
+# users = [
+#     {"name": "John Doe", "birthday": "1985.01.30"},
+#     {"name": "Tolik", "birthday": "2004.02.29"},
+#     {"name": "Anna", "birthday": "1979.02.28"},
+#     {"name": "Olha", "birthday": "1993.03.06"},
+#     {"name": "Andrii", "birthday": "1993.03.02"},
+#     {"name": "Oleh", "birthday": "1993.03.03"},
+#     {"name": "Jane Smith", "birthday": "1992.03.07"}
+# ]
 
-print(get_upcoming_birthdays(users))
-# Визначте різницю між днем народження та поточним днем для визначення днів народження на наступний тиждень. (7днів)
-# Створіть структуру даних, яка зберігатиме ім'я користувача та відповідну дату привітання,
-# якщо день народження відбувається протягом наступного тижня.
+# upcoming_birthdays = get_upcoming_birthdays(users)
+# print(upcoming_birthdays)
